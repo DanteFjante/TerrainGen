185a2d14b8591980f59e5430747ed33b7f196dc0
Dante <dantecavallin@gmail.com>
2021-03-11 17:46:29 +0100

    Revert "Added support for multiple combine types in noise job."
    This reverts commit 59215deec0c813b6ba1780c188e7dd1eb063104d.

---
+++ Assets/Scripts/Jobs/CombineVerticesJob.cs
@@ -0,0 +1,12 @@
+using Unity.Jobs;
+
+namespace Worldgen
+{
+    public struct CombineVerticesJob : IJobParallelFor
+    {
+        public void Execute(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
